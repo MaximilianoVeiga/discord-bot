0e124f31c1c7a0616187732e7e3cf4cf2766e217
Maximiliano Neto <maxsantosneto@gmail.com>
2021-06-24 21:41:45 -0300

    parte-4

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
 'use strict';
 
-require('dotenv').config();
+const Discord = require('discord.js');
 
-const { Client, MessageEmbed } = require('discord.js');
-const client = new Client();
-
-const DISCORD_BOT_SECRET = process.env.DISCORD_BOT_SECRET;
-const PREFIX = process.env.PREFIX;
+const client = new Discord.Client();
 
 client.on('ready', () => {
     console.info('Aplicativo aberto!');
 });
 
 client.on('message', message => {
-    if (!message.content.startsWith(PREFIX) || message.author.bot) return;
-
-    const command = message.content.split(PREFIX)[1];
-
     if (command === 'olá') {
         message.channel.send('mundo');
-    } else if (command === 'mensagem colorida') {
-        const embed = new MessageEmbed()
-            .setTitle('Título maneiro')
-            .setColor('0xff0000')
-            .setDescription('Olá, tudo bem?\nOutra linha.')
-            .setAuthor('Eu')
-
-        message.channel.send(embed);
     }
 });
 
-client.login(DISCORD_BOT_SECRET);
+client.login('ODU3NDE5ODY3ODI0MTkzNTQ2.YNPUeg.jYGqcwy28Td-jHdOvGMU89xL5sk');
